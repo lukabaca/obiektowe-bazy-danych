c4feb199a4f26eb781edbe80c96d7299eb3f6719
lukabaca <lkszblaszczyk@gmail.com>
2019-01-12 15:24:06 +0100

    working package of adding data

--- projekt.sql
+++ projekt.sql
@@ -303,15 +303,68 @@ commit;
 
 
 create or replace package package_addRecord as
-    PROCEDURE addContact(id in integer, telephoneNumber in varchar2, email in varchar2);
-    PROCEDURE addRole(id in integer, name in varchar2);
-    PROCEDURE addUser(id in integer, name in varchar2, surname in varchar2, birthDate in date, pesel in varchar2,
-    document_id in varchar2, idContact in integer, idRole in integer, recordings in k_recording);
-    PROCEDURE addReservation(id in integer, idUser in integer, startDate in date, endDate in date, 
+    PROCEDURE addContact(contactId in integer, telephoneNumber in varchar2, email in varchar2);
+    PROCEDURE addRole(roleId in integer, name in varchar2);
+    PROCEDURE addUser(userId in integer, name in varchar2, surname in varchar2, birthDate in date, pesel in varchar2,
+    document_id in varchar2, contactId in integer, roleId in integer, recordingCollection in k_recording);
+    PROCEDURE addReservation(reservationId in integer, userId in integer, startDate in date, endDate in date, 
     cost in number, byTimeReservationType in number, description in varchar2);
-    PROCEDURE addKart(id in integer, availability number, prize in number, name in varchar2, descripiton in varchar2);
-    PROCEDURE addRecording(id in integer, idUser in integer, recordingLink in varchar2, title in varchar2);
-    PROCEDURE addReservationKart(idReservation in integer, idKart in integer);
-    PROCEDURE addLap(id in integer, idUser in integer, idKart in integer, averageSpeed in number, lapDate in date,
-    minute in integer, second in integer, milisecond in integer);
+    PROCEDURE addKart(kartId in integer, availability number, prize in number, name in varchar2, descripiton in varchar2);
+    PROCEDURE addReservationKart(reservationId in integer, kartId in integer);
+    PROCEDURE addLap(lapId in integer, userId in integer, kartId in integer, averageSpeed in number, lapDate in date,
+    lapMinute in integer, lapSecond in integer, lapMilisecond in integer);
+
 end package_addRecord;
+
+CREATE OR REPLACE
+PACKAGE BODY PACKAGE_ADDRECORD AS
+
+PROCEDURE addContact(contactId in integer, telephoneNumber in varchar2, email in varchar2) AS
+  BEGIN
+    insert into contact values(contactId, telephoneNumber, email);
+  END addContact;
+
+  PROCEDURE addRole(roleId in integer, name in varchar2) AS
+  BEGIN
+    insert into role values(roleId, name);
+  END addRole;
+
+ PROCEDURE addUser(userId in integer, name in varchar2, surname in varchar2, birthDate in date, pesel in varchar2,
+    document_id in varchar2, contactId in integer, roleId in integer, recordingCollection in k_recording) AS
+  BEGIN
+    insert into usr select userId, name, surname, birthDate, 
+    pesel, document_id, ref(contactRef), ref(rolRef), recordingCollection from role rolRef, contact contactRef 
+    where rolRef.id = roleId and contactRef.id = contactId;
+  END addUser;
+  
+   PROCEDURE addReservation(reservationId in integer, userId in integer, startDate in date, endDate in date, 
+    cost in number, byTimeReservationType in number, description in varchar2) AS
+  BEGIN
+    insert into reservation select reservationId, ref(usrRef), startDate, endDate, 
+    cost, byTimeReservationType, description
+    from usr usrRef where usrRef.id = userId;
+  END addReservation;
+
+  PROCEDURE addKart(kartId in integer, availability number, prize in number, name in varchar2, descripiton in varchar2) AS
+  BEGIN
+   insert into kart values(kartId, availability, prize, name, descripiton);
+  END addKart;
+
+
+  PROCEDURE addReservationKart(reservationId in integer, kartId in integer) AS
+  BEGIN
+    insert into reservationKart select 
+    ref(reserRef), ref(kartRef) from reservation reserRef, kart kartRef
+    where reserRef.id = reservationId and kartRef.id = kartId;
+  END addReservationKart;
+
+
+  PROCEDURE addLap(lapId in integer, userId in integer, kartId in integer, averageSpeed in number, lapDate in date,
+    lapMinute in integer, lapSecond in integer, lapMilisecond in integer) AS
+  BEGIN
+    insert into lap select lapId, ref(usrRef), ref(kartRef),
+    averageSpeed, lapDate, lapMinute, lapSecond, lapMilisecond from usr usrRef, kart kartRef
+    where usrRef.id = userId and kartRef.id = kartId;
+  END addLap;
+  
+END PACKAGE_ADDRECORD;
