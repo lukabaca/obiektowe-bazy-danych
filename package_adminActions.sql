ed0aebb076a2781d7f0cf02dbff378a7ecde8056
lukabaca <lkszblaszczyk@gmail.com>
2019-01-13 14:39:50 +0100

    skonczony pakiet admina

--- package_adminActions.sql
+++ package_adminActions.sql
@@ -1,6 +1,10 @@
 create or replace package package_adminActions as
     procedure changeUserRole(userId in integer, roleId in integer);
-    procedure changeKartAvailability(kartId in integer, availability in number); 
+    procedure changeKartAvailability(kartId in integer, kartAvailability in number); 
+    
+    userNotFoundException exception;
+    roleNotFoundExceptiom exception;
+    kartNotFoundException exception;
 end package_adminActions;
 
 CREATE OR REPLACE
@@ -8,11 +12,9 @@ PACKAGE BODY PACKAGE_ADMINACTIONS AS
   
   procedure changeUserRole(userId in integer, roleId in integer) AS
   
-  userNotFoundException exception;
-  roleNotFoundExceptiom exception;
-  
   roleRef ref t_role;
   roleNameTmp varchar2(15);
+  usrEditedName varchar2(40);
   BEGIN
     if (not PACKAGE_CHECKINGRECORDEXIST.isUserFound(userId)) then
         raise userNotFoundException;
@@ -20,9 +22,11 @@ PACKAGE BODY PACKAGE_ADMINACTIONS AS
         raise roleNotFoundExceptiom;
     else 
         select ref(roleRefTmp) into roleRef from role roleRefTmp where roleRefTmp.id = roleId;
-        update usr set role = roleRef;
+        update usr set role = roleRef
+        where usr.id = userId;
         select role.name into roleNameTmp from role where role.id = roleId;
-        DBMS_OUTPUT.PUT_LINE('Zmieniono role na role: ' || roleNameTmp);
+        select usr.name into usrEditedName from usr where usr.id = userId;
+        DBMS_OUTPUT.PUT_LINE('Zmieniono role uzytkownikowi: ' || usrEditedName || ' na role: ' || roleNameTmp);
     end if;
   EXCEPTION
     when userNotFoundException then
@@ -31,15 +35,43 @@ PACKAGE BODY PACKAGE_ADMINACTIONS AS
         DBMS_OUTPUT.PUT_LINE('Nie znaleziono roli o podanym ID');
   END changeUserRole;
 
-  procedure changeKartAvailability(kartId in integer, availability in number) AS
+  procedure changeKartAvailability(kartId in integer, kartAvailability in number) AS
+  kartEditedName varchar2(30);
+  
+  kartAvailabiltyName varchar2(20);
   BEGIN
-    -- TODO: Implementation required for procedure PACKAGE_ADMINACTIONS.changeKartAvailability
-    NULL;
+    if (not PACKAGE_CHECKINGRECORDEXIST.isKartFound(kartId)) then
+        raise kartNotFoundException;
+    else
+        update kart set availability = kartAvailability where kart.id = kartId;
+        select k.name into kartEditedName from kart k where k.id = kartId;
+        if kartAvailability = 1 then
+            kartAvailabiltyName:= 'Dostêpny';
+        else
+            kartAvailabiltyName:= 'Niedostêpny';
+        end if;
+        DBMS_OUTPUT.PUT_LINE('Zmienio dostepnosc dla gokartu: ' || kartEditedName || ' na dostêpnosc: ' ||
+        kartAvailabiltyName);    
+    end if;
+   EXCEPTION
+       when kartNotFoundException then
+        DBMS_OUTPUT.PUT_LINE('Nie znaleziono gokartu o podanym ID');    
   END changeKartAvailability;
 
 END PACKAGE_ADMINACTIONS;
 
 set serveroutput on;
+
+
+
+
+
+
+
+
+
+
+/*-----------------------------------------------------*/
 
 /*testowanie dzialania */
 
@@ -47,12 +79,27 @@ DECLARE
   USERID NUMBER;
   ROLEID NUMBER;
 BEGIN
-  USERID := 1;
-  ROLEID := 1;
+  USERID := 2;
+  ROLEID := 5;
 
   PACKAGE_ADMINACTIONS.CHANGEUSERROLE(
     USERID => USERID,
     ROLEID => ROLEID
+  );
+--rollback; 
+END;
+
+
+DECLARE
+  KARTID NUMBER;
+  KARTAVAILABILITY NUMBER;
+BEGIN
+  KARTID := 1;
+  KARTAVAILABILITY := 0;
+
+  PACKAGE_ADMINACTIONS.CHANGEKARTAVAILABILITY(
+    KARTID => KARTID,
+    KARTAVAILABILITY => KARTAVAILABILITY
   );
 --rollback; 
 END;
